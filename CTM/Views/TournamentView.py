6a9680b3a5b75e5d4287c2896593ad030e912719
EL-Yasmina <yasminataif99@gmail.com>
2024-04-30 13:12:46 +0200

    👌 IMPROVE:correction d'erreurs de dossier Views

--- CTM/Views/TournamentView.py
+++ CTM/Views/TournamentView.py
@@ -1,9 +1,9 @@
 from Views.View import View
 
 
-
 class TournamentView(View):
     def __init__(self):
+
         self.description = None
         self.rounds = None
         self.end_date = None
@@ -11,8 +11,8 @@ class TournamentView(View):
         self.location = None
         self.name = None
 
-
     def get_tournament_info(self):
+
         """
         Demande à l'utilisateur de fournir les informations pour un nouveau tournoi.
         Effectue des validations pour s'assurer que les entrées sont correctes.
@@ -27,8 +27,8 @@ class TournamentView(View):
         # Appel de la méthode présente dans View.py
         self.start_date = self.get_valid_date_input("Date de début (JJ/MM/AAAA): ")
         self.end_date = self.get_valid_date_input("Date de fin (JJ/MM/AAAA): ")
-
-        self.rounds = self.get_valid_int_input("Nombre de tours (optionnel, défaut à 4): ", 4)  # IF EMPTY, SET TO 4
+        # IF EMPTY, SET TO 4
+        self.rounds = self.get_valid_int_input("Nombre de tours (optionnel, défaut à 4): ", 4)
         self.description = input("Description du tournoi (optionnel): ")
 
         return self.name, self.location, self.start_date, self.end_date, self.rounds, self.description
